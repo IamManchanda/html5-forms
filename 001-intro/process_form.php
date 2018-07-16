396406887e305bbfe3171b49198c4d7a447e37ca
Harry Manchanda <harmanmanchanda182@gmail.com>
2018-07-16 12:34:36 +0530

    Add intro section

---
+++ 001-intro/process_form.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Simple Form Example</title>
+  </head>
+  <body>
+
+    <h1>Simple form example</h1>
+
+    You submitted: <?php echo $_GET['forename']; ?>
+
+  </body>
+</html>
